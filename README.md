549e3a074aa5538efa25787e2e1eb9b1cd38814b
waregin <warebec@gmail.com>
2024-01-21 16:36:24 -0500

    got initial url list scrape from chewy

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Python learning project
 
 Web scraper for chewy.com to do price comparison and find options
 that work for my pets, such as not containing any allergens.
+
+Sources:
+- https://realpython.com/beautiful-soup-web-scraper-python/
+- https://realpython.com/python-web-scraping-practical-introduction/
+- https://oxylabs.io/blog/python-web-scraping
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+# Press Shift+F10 to execute
+URL = "https://www.chewy.com/brands/fussie-cat-6778"
 
-print(page.text)
+driver = webdriver.Chrome()
+driver.get(URL)
+
+content = driver.page_source
+soup = BeautifulSoup(content, 'html.parser')
+
+product_cards = soup.find_all(attrs={"class": "kib-product-card__content"})
+
+product_urls = []
+for element in product_cards:
+    print(element)
+
+    url = element.find("a")["href"]
+    if url not in product_urls:
+        product_urls.append(url)
+
+for product_url in product_urls:
+    print(product_url)
+
+driver.quit()  # closing the browser
